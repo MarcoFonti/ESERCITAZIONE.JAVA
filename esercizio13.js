c1db16f0a4def640745fc79bddc525510dd0a66e
Marco Fonti <fontimarco_2022@libero.it>
2023-10-25 19:13:48 +0200

    esercizio restituzione media

---
+++ esercizio13.js
@@ -0,0 +1,34 @@
+//Crea una funzione chiamata ageAverage
+//Restituisca l'età media.
+
+
+function ageAverage (persons) {
+  if (persons.length === 0) {
+    return 0;
+
+  }
+
+  const totalAge = persons.reduce ( (acc, person) => acc + person.age, 0 );
+  const average = totalAge / persons.length;
+  return average;
+
+}
+
+const persons = [
+
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+  
+];
+
+const average = ageAverage(persons);
+console.log(persons);
+console.log(average);
